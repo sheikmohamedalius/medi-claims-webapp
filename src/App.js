fad1c7868b790dfa291768a364c4386a1bd32bc7
sheikmohamedalius <sheik49@gmail.com>
2025-09-01 01:24:36 +0530

    yml file added for s3 with cloudfront

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload react application.
+          Edit <code>src/App.js</code> and save to reload react new application.
         </p>
         <a
           className="App-link"
